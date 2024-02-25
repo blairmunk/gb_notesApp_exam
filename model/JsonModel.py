992b66171b32e5954b38564ec20f16d75935f5e9
Alexander Lemport <lemportpro@mail.ru>
2024-02-25 11:36:31 +0300

    Some refactoring

--- model/JsonModel.py
+++ model/JsonModel.py
@@ -1,7 +1,7 @@
 import json
-from model import Note
 from datetime import datetime
-from InterfaceNotes import InterfaceNotes
+from .InterfaceNotes import InterfaceNotes
+from .Note import Note
 
 class NoteManager(InterfaceNotes):
     def __init__(self, filename):
@@ -70,22 +70,3 @@ class NoteManager(InterfaceNotes):
             print(f"Content: {note.content}")
             print()
 
-# Пример использования
-note_manager = NoteManager("notes.json")
-
-# Добавление заметок
-note_manager.add_note("Заголовок заметки 1", "Содержимое заметки 1")
-note_manager.add_note("Заголовок заметки 2", "Содержимое заметки 2")
-note_manager.add_note("Заголовок заметки 3", "Содержимое заметки 3")
-
-note_manager.edit_note_by_title("Заголовок заметки 2", "Новый заголовок", "Новое содержимое")
-
-
-# Удаление заметки по заголовку
-note_manager.delete_note_by_title("Заголовок заметки 2")
-
-# Сохранение и обновление JSON-файла
-note_manager.save_notes()
-
-# Вывод всех заметок на консоль
-note_manager.print_all_notes()
---
+++ view/ConsoleView.py
@@ -0,0 +1,4 @@
+
+
+class ConsoleView():
+    pass
