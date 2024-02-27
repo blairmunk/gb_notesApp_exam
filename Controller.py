b4d08d1b80786c8c9ccac6ad85da98e378c056d3
Alexander Lemport <lemportpro@mail.ru>
2024-02-27 13:57:22 +0300

    Added print_by_title feature

--- Controller.py
+++ Controller.py
@@ -16,19 +16,38 @@ class Controller():
                 content = input("Введите текст заметки: ")
                 self.model.add_note(title, content)
                 continue
+            
             elif decision.lower() in {"2", "e"}:
                 title = input("Введите заголовок заметки: ")
+                if not self.model.check_note_by_title(title):
+                    print("Заметка с таким заголовком не найдена!")
+                    continue
+                print("Вот эта заметка:")
+                self.model.print_note_by_title(title)
+                print()
+
                 new_title = input("Введите новый заголовок для заметки: ")
                 new_content = input("Введите текст заметки: ")
-                self.model.edit_note(title, new_title, new_content)
+                self.model.edit_note_by_title(title, new_title, new_content)
+                print("Заметка отредактирована!")
+                print()
                 continue
+            
             elif decision.lower() in {"3", "d"}:
                 title = input("Введите заголовок заметки, которая должна быть удалена: ")
-                self.model.delete_note(title)
+                self.model.delete_note_by_title(title)
                 continue
+            
             elif decision.lower() in {"4", "a"}:
+                print("Все ваши заметки выведены ниже")
                 self.model.print_all_notes()
                 continue
+            
+            elif decision.lower() in {"5", "f"}:
+                title = input("Введите заголовок заметки: ")
+                self.model.print_note_by_title(title)
+                continue
+            
             else: 
                 print("Выход. Пока!")
                 break
--- model/JsonModel.py
+++ model/JsonModel.py
@@ -48,6 +48,9 @@ class NoteManager(InterfaceNotes):
             note = Note(title, content)
             self.notes.append(note)
             self.save_notes()
+        print()
+        print(f'Заметка c заголовком "{title}" создана!')
+        print()
 
     def delete_note_by_title(self, title):
         self.notes = [note for note in self.notes if note.title != title]
@@ -58,6 +61,20 @@ class NoteManager(InterfaceNotes):
             if note.title == title:
                 return note
         return None
+    
+    def print_note_by_title(self,title):
+        if self.check_note_by_title(title):
+            note = self.get_note_by_title(title)
+        else: 
+            print("Заметка с таким заголовком не найдена!")
+            return 
+        print(f"Created_at: {note.created_at.strftime('%Y-%m-%d %H:%M')}")
+        print(f"Modified_at: {note.created_at.strftime('%Y-%m-%d %H:%M')}")
+        print(f"ID: {note.id}")
+        print(f"Title: {note.title}")
+        print(f"Content: {note.content}")
+        print()
+        
 
     def edit_note_by_title(self, title, new_title, new_content):
         note = self.get_note_by_title(title)
@@ -65,13 +82,13 @@ class NoteManager(InterfaceNotes):
             note.title = new_title
             note.modified_at = datetime.now()
             note.content = new_content
+        
+
 
     def print_all_notes(self):
         for note in self.notes:
             print(f"Created_at: {note.created_at.strftime('%Y-%m-%d %H:%M')}")
-            print(f"Modified_at: {note.created_at.strftime('%Y-%m-%d %H:%M')}")
             print(f"ID: {note.id}")
             print(f"Title: {note.title}")
-            print(f"Content: {note.content}")
             print()
 
